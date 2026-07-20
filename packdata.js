5c0575654d1cd09314e22f38d9433123046f843d
TwoPaths27 <worldsundersiege@gmail.com>
2026-07-20 09:55:04 -0400

    Update packdata.js

--- packdata.js
+++ packdata.js
@@ -1373,8 +1373,8 @@ const BOA_PACK_CONFIG = {
   uncommonsPerPack: 4,
   premiumSlotsPerPack: 2,
   premiumOdds: {
-    secret: 1 / 48,
-    ultraRare: 1 / 8,
-    superRare: 1 / 4
+    secret: 1 / 96,
+    ultraRare: 1 / 16,
+    superRare: 1 / 8
   }
 };
