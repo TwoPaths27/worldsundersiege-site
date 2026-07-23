a4e5bc569be74c47e8d7919efc1cc232247b41dc
TwoPaths27 <worldsundersiege@gmail.com>
2026-07-23 06:51:21 -0400

    Add files via upload

--- open-packs.js
+++ open-packs.js
@@ -102,6 +102,7 @@
   const soundPaths = Object.freeze({
     packRip: "sounds/pack-rip.mp3",
     packPop: "sounds/card-flip.mp3",
+    purchase: "sounds/drop-coin.mp3",
     cardFlip: "sounds/card-flip.mp3",
     superRare: "sounds/super-rare.mp3",
     ultraRare: "sounds/ultra-rare.mp3",
@@ -160,7 +161,8 @@
       return;
     }
 
-    playSound(soundPaths.cardFlip, SOUND_VOLUME, 0.98 + Math.random() * 0.04);
+    playSound(soundPaths.cardFlip,
+      soundPaths.purchase, SOUND_VOLUME, 0.98 + Math.random() * 0.04);
   }
 
   function preloadSounds() {
@@ -168,6 +170,7 @@
       soundPaths.packRip,
       soundPaths.packPop,
       soundPaths.cardFlip,
+      soundPaths.purchase,
       soundPaths.superRare,
       soundPaths.ultraRare,
       ...Object.values(soundPaths.secretRare)
@@ -218,6 +221,9 @@
       return;
     }
     showEconomyMessage(`${cost.toLocaleString()} Gold spent. Your purchase is saved until it is fully opened.`);
+    if (cost > 0) {
+      playSound(soundPaths.purchase, SOUND_VOLUME);
+    }
     refreshGold();
     restoreOpening(result.opening);
   }
